5724ad0dd3777fef62431eea85c6b0a0dd85328d
ih551311 <ih551311@gmail.com>
2026-04-07 12:04:05 +0900

    점프맵 만들고 한글

---
+++ Assets/Scripts/CubeagameUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine. UI;
+using TMPro;
+
+public class CubeagameUI : MonoBehaviour
+{
+public TextMeshProUGUI TimerText;
+        public float Timer;
+
+        void start ()
+    {
+
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        TimerText.text = "생존시간" + Timer.ToString("0.00");
+    }
+}
